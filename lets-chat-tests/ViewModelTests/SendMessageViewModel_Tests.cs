7b0837f06b521aa6034bb92436f321e49d77d7ca
James.Briaris <James.Briaris@civica.co.uk>
2017-01-25 16:18:49 +0000

    Added testing framework. Tests still to be completed.

---
+++ lets-chat-tests/ViewModelTests/SendMessageViewModel_Tests.cs
@@ -0,0 +1,30 @@
+﻿using lets_chat;
+using lets_chat.ViewModels;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace lets_chat_tests
+{
+    [TestFixture]
+    public class SendMessageViewModel_Tests
+    {
+        private IMessageService _messageService;
+        private ISendMessageViewModel _viewModel;
+
+        [SetUp]
+        public void TestInit()
+        {
+            _messageService = Substitute.For<IMessageService>();
+            _viewModel = new SendMessageViewModel(_messageService);
+        }
+
+        [Test]
+        public void SendMessageViewModel_OnSendMessageCommand_CallsSendMessageOnMessageService()
+        {
+            const string ExpectedMsg = "Test";
+            _viewModel.SendMessageCommand.Execute(ExpectedMsg);
+            _messageService.Received(1).SendMessage(ExpectedMsg);
+        }
+    }
+
+}
